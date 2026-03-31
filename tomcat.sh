691cede6006310ad9f34117f1a2a104cc21cf5e1
Saikrishnadevsecops <saikrishna.studymaterial@gmail.com>
2026-03-31 12:29:18 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,5 +1,5 @@
 yum install java-17-amazon-corretto -y
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v10.1.26/bin/apache-tomcat-10.1.26.tar.gz
+wget https://archive.apache.org/dist/tomcat/tomcat-10/v10.1.26/bin/apache-tomcat-10.1.26.tar.gz
 tar -zxvf apache-tomcat-10.1.26.tar.gz
 sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-10.1.26/conf/tomcat-users.xml
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-10.1.26/conf/tomcat-users.xml
